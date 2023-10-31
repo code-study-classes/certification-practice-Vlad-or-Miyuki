9ef649d3efbe1475bbfefc472d0ed8ef6fb50355
Miyuki <7001vlbl@bk.ru>
2023-10-31 13:44:34 +0300

    сделал 2 задания в numbers

--- numbers.js
+++ numbers.js
@@ -1,1 +1,8 @@
+export const calculateDistance = (x1, x2) => Math.abs(x2 - x1);
 
+export const calculateSegmentProduct = (a, b, c) => {
+  const lengthAC = Math.abs(c - a);
+  const lengthBC = Math.abs(b - c);
+  const result = lengthAC * lengthBC;
+  return result;
+};
