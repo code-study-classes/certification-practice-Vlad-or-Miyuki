baa7067c3aac5167a9ed092cd7b74c983cbeb72e
Miyuki <7001vlbl@bk.ru>
2023-11-09 14:43:31 +0300

    задание 1 в strings

---
+++ strings.js
@@ -0,0 +1,36 @@
+export const countUppercaseLetters = (str) => {
+    const uppercaseLetters = str.match(/[A-Z]/gu);
+    return uppercaseLetters ? uppercaseLetters.length : 0;
+  };
+
+export const combineStrings = () => {
+
+};
+
+export const containsSubstring = () => {
+
+};
+
+export const replaceSubstring = () => {
+
+};
+
+export const countWordsWithA = () => {
+
+};
+
+export const normalizeSpaces = () => {
+
+};
+
+export const extractFileName = () => {
+
+};
+
+export const encryptSentence = () => {
+
+};
+
+export const checkBrackets = () => {
+
+};
